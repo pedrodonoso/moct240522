2f8cfa0f478078d9e87431ead68e78924eaf6ef5
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-06-12 09:21:26 -0400

    analysis and script updated

--- script.sh
+++ script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+sudo rm -rf ./c_5r7_*
 sudo make build-moct
-echo "==== REFINAMIENTO INICIAL ===="
-./mesher_roi -d ./data/cortex.mdl -s 5 -r ./data/cortex_surf_roi.mdl 7 -u c_5r7_0 -m -v -t 0.0
-for i in {0..20}
+threshold=(0.03)
+for i in {0..5}
 do
+  new=$(($i-1))
   echo "==== ITERACION $i ===="
-  new=$(($i+1))
-  echo "var : $new"
-  ./mesher_roi -d ./data/cortex.mdl -c ./c_5r7_$i.oct -l ./c_5r7_$i.ref -u c_5r7_$new -m -v -t 0.0
+  if (( $i == 0 )); then
+    ./mesher_roi -d ./data/cortex.mdl -s 5 -r ./data/cortex_surf_roi.mdl 7 -u c_5r7_$i -m -v -t $threshold &> ./c_5r7_$i.log
+  else
+    ./mesher_roi -d ./data/cortex.mdl -c ./c_5r7_$new.oct -l ./c_5r7_$new.ref -u c_5r7_$i -m -v -t $threshold &> ./c_5r7_$i.log
+  fi
 done
